02c2e9d7f0badca809c92735c0e4d2c6e1e22a4a
Jayden Shelderfer <jayshelderfer@gmail.com>
2025-07-06 14:22:12 -0400

    deployment-ready6

--- server/index.js
+++ server/index.js
@@ -3,9 +3,6 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import analyzeRoute from './routes/analyze.js';
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-
 dotenv.config();
 
 const app = express();
@@ -13,16 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/analyze', analyzeRoute);
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
